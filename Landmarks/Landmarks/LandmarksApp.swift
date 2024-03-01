353c0a1dd984362d0354a6c95701b7c585195cb3
wasesasegara <wasesasegara@gmail.com>
2024-03-02 01:08:39 +0700

    Init Landmarks project

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Bisma S Wasesasegara on 02/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
