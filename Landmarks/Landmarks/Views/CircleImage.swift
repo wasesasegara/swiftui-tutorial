36869f7ca9e04950221ab5c1fb114906efd306f4
wasesasegara <wasesasegara@gmail.com>
2024-03-02 01:22:45 +0700

    Create a custom image view

---
+++ Landmarks/Landmarks/Views/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Bisma S Wasesasegara on 02/03/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
