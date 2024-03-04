09f6b5b86d892ba8d4587d3893415fd54cb38367
wasesasegara <wasesasegara@gmail.com>
2024-03-04 18:31:49 +0700

    Display a user profile

---
+++ Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Bisma S Wasesasegara on 04/03/24.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+            .environmentObject(ModelData())
+    }
+}
